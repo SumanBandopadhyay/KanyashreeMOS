c3954d13d17488f8c9f1112df8ed0e0e4272f75a
PoulamiBhose <bhosepoulami@gmail.com>
2016-08-13 20:33:03 +0530

    Initial commit

---
+++ app/src/main/java/com/kanyashreemos/VideoGallery.java
@@ -0,0 +1,13 @@
+package com.kanyashreemos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VideoGallery extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_gallery);
+    }
+}
